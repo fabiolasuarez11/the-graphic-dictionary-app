9cda66bbdaf8ff3c626dec807abec97f45fdd3d2
Fabiola Suarez <fabiola.marcello@gmail.com>
2022-08-30 19:08:50 -0400

    Results Synonyms

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,22 +6,46 @@ export default function Results(props) {
 		return (
 			<div className="Results">
 				<div className="card m-5">
-					<h2>{props.definitions.word}</h2>
+					<div className="card-header">
+						<h2>{props.definitions.word}</h2>
+					</div>
 					{props.definitions.meanings.map((meaning, index) => {
-						// return meaning.definitions[0].definition;
-						//-05:42 in Meanings video
 						return (
 							<div key={index}>
 								<h3>{meaning.partOfSpeech}</h3>
 								{meaning.definitions.map((definition, index) => {
-									return (
-										<div key={index}>
-											<p>
-												{definition.definition} <br />
-												<em>{definition.example}</em>
-											</p>
-										</div>
-									);
+									if (definition.definition) {
+										return (
+											<div key={index}>
+												<h5>Definition</h5>
+												<p>{definition.definition} </p>
+											</div>
+										);
+									}
+									if (definition.example) {
+										return (
+											<div key={index}>
+												<h5>Example</h5>
+												<p>
+													<em>{definition.example}</em>
+												</p>
+											</div>
+										);
+									}
+									if (definition.synonyms) {
+										return (
+											<div>
+												<h5>Synonym</h5>
+												<ul>
+													{definition.synonyms.map((synonym, index) => {
+														return <li key={index}>{synonym}</li>;
+													})}
+												</ul>
+											</div>
+										);
+									} else {
+										return null;
+									}
 								})}
 							</div>
 						);
