71123301e3924fdcefa0b1259ac39341ed4b40e4
Fabiola Suarez <fabiola.marcello@gmail.com>
2022-09-07 17:26:22 -0400

    Function Fix

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -6,7 +6,7 @@ export default function Photos(props) {
 		return (
 			<div className="Photos">
 				<div className="row">
-					{props.photos.map(function (photo, index) {
+					{props.photos.map((photo, index) => {
 						return (
 							<div className="col-4" key={index}>
 								<a href={photo.src.original} target="_blank" rel="noreferrer">
